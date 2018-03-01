451cac4a9b72bd2099166c43ad3a9a00c70e2300
tange4315 <tange4315@HRH-IC0027336.wrdsb.ca>
2018-03-01 10:47:26 -0500

    working on assignment two

---
+++ src/Assignment4/A4Q4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tange4315
+ */
+public class A4Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // create a Scanner to read information
+        Scanner input = new Scanner(System.in);
+        
+        
+        
+    }
+}
