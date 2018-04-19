6401fd45b47fc8ea1c2c65a980e93f6e7b58aae2
tange4315 <tange4315@HRH-IC0027336.wrdsb.ca>
2018-04-19 11:18:57 -0400

    working on assignment six

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -36,12 +36,21 @@ public class A6Q7 {
             prime[i] = true;
         }
         
-        for (int p = 2; p < numbers.length; p++) {
-            // go through array and look for prime numbers
-            for (int i = 0; i < numbers.length; i++) {
-                // mark all composite numbers
-                if (numbers[i] % p == 0) {
-                    prime[i] = false;
+        // initially let p equal 2
+        int p = 2;
+        
+        // go through array and look for composite numbers
+        for (int i = 0; i < numbers.length; i++) {
+            // mark all composite numbers in array
+            if (numbers[i] % p == 0) {
+                prime[i] = false;
+            }
+            // do not let p equal anything that's marked as composite
+            for (int j = i; j < numbers.length; j++) {
+                if (prime[j] == false) {
+                    j++;
+                } else {
+                    p = numbers[j];
                 }
             }
         }
@@ -49,9 +58,9 @@ public class A6Q7 {
         // tell user the prime numbers
         System.out.println("The prime numbers throughout 2 to 1000 are: ");
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             if (prime[i] == true) {
-                System.out.print(prime[i] + " ");
+                System.out.print(numbers[i] + " ");
             }
         }
         
