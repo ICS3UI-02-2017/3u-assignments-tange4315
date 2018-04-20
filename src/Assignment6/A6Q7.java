5c1b2a1145071152c30f44ca7d92be7dbbe207e6
tange4315 <tange4315@HRH-IC0027336.wrdsb.ca>
2018-04-20 11:44:11 -0400

    working on assignment six

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -16,53 +16,52 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // create a Scanner to read information
         Scanner input = new Scanner(System.in);
-        
+
         // create an array to represent numbers 0 to 1000
         int numbers[] = new int[1001];
-        
+
         // fill the array with consecutive integers
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i;
         }
-        
+
         // create an array to store booleans to determine prime numbers
         boolean prime[] = new boolean[1001];
-        
+
         // assume all numbers are prime
         for (int i = 0; i < prime.length; i++) {
             prime[i] = true;
         }
-        
-        // initially let p equal 2
-        int p = 2;
-        
-        // go through array and look for composite numbers
-        for (int i = 0; i < numbers.length; i++) {
-            // mark all composite numbers in array
-            if (numbers[i] % p == 0) {
-                prime[i] = false;
-            }
-            // do not let p equal anything that's marked as composite
-            for (int j = i; j < numbers.length; j++) {
-                if (prime[j] == false) {
-                    j++;
-                } else {
-                    p = numbers[j];
+
+        // initially let p equal 2 which is the first prime number
+        for (int p = 2; p < numbers.length; p++) {
+            // go through array and look for composite numbers
+            for (int i = 0; i < numbers.length; i++) {
+                // mark all composite numbers in array
+                if (numbers[i] % p == 0) {
+                    prime[i] = false;
+                }
+                // do not let p equal anything that's marked as a composite number
+                for (int j = p; j < numbers.length; j++) {
+                    if (prime[j] == false){
+                        j++;
+                    } else {
+                        p = numbers[j];
+                    }
                 }
             }
         }
         
         // tell user the prime numbers
         System.out.println("The prime numbers throughout 2 to 1000 are: ");
-        
+
         for (int i = 0; i < numbers.length; i++) {
             if (prime[i] == true) {
                 System.out.print(numbers[i] + " ");
             }
         }
-        
     }
 }
