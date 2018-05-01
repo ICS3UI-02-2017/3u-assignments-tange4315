deb3324686da2d628156e42c95bc8f3d04ef27f5
tange4315 <tange4315@HRH-IC0027336.wrdsb.ca>
2018-05-01 10:40:28 -0400

    finished assignment seven

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -15,11 +15,28 @@ public class A7Q8 {
      */
     
     // create a method that returns whether every digit of a positive integer is odd
-    public static void allDigitsOdd(int number) {
-        
+    public static boolean allDigitsOdd(int number) {
+        // parse integer into a String
+        String numberString = Integer.toString(number);
+        // create a boolean variable assuming that all numbers in the integer are odd
+        boolean odd = true;
+        // use a for loop to go through the number
+        for (int i = 0; i < numberString.length(); i++) {
+            // detect an even number
+            if (numberString.charAt(i) == '0' ||
+                    numberString.charAt(i) == '2' ||
+                    numberString.charAt(i) == '4' ||
+                    numberString.charAt(i) == '6' ||
+                    numberString.charAt(i) == '8') {
+                // mark odd if a number in the integer is even
+                odd = false;
+            }
+        }
+        // return boolean value to user to determine whether if all digits were odd
+        return odd;
     }
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        allDigitsOdd(1236);
     }
 }
