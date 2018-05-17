1fc4e0baaaa7e962ca9c5ea97e94167e6f665be9
tange4315 <tange4315@HRH-IC0027336.wrdsb.ca>
2018-05-17 11:17:52 -0400

    working on assignment eight

--- src/Assignment8/A8Q1.java
+++ src/Assignment8/A8Q1.java
@@ -33,9 +33,9 @@ public class A8Q1 extends JComponent implements ActionListener {
     // timer used to run the game loop
     // this is what keeps our time running smoothly :)
     Timer gameTimer;
-    
+   
     // YOUR GAME VARIABLES WOULD GO HERE
-    
+   
     // make colours
     Color skin = new Color(252, 213, 169);
     Color purple = new Color(122, 13, 155);
@@ -53,7 +53,29 @@ public class A8Q1 extends JComponent implements ActionListener {
     int hatRectX = 315;
     int hatOvalX = 245;
     int hatStripeRectX = 315;
+    // Y coordinates for the hat
+    int hatRectY = 60;
+    int hatOvalY = 215;
+    int hatStripeRectY = 180;
     // make a boolean to move the hat
+    boolean hatMoveRight = true;
+    boolean hatMoveUp = true;
+    
+    // X coordinates for the left eye
+    int leftEyeX = 300;
+    int leftLaserX = 310;
+    // make a boolean to move the left eye
+    boolean leftEyeMoveIn = true;
+    
+    // Y coordinates for the right eye
+    int rightEyeX = 450;
+    int rightLaserX = 460;
+    // make a boolean to move the right eye
+    boolean rightEyeMoveIn = true;
+    
+    // mouse variables
+    int mouseX = 0;
+    int mouseY = 0;
 
     // GAME VARIABLES END HERE
     
@@ -92,6 +114,7 @@ public class A8Q1 extends JComponent implements ActionListener {
     // NOTE: This is already double buffered!(helps with framerate/speed)
     @Override
     public void paintComponent(Graphics g) {
+        
         // always clear the screen first
         g.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -105,18 +128,18 @@ public class A8Q1 extends JComponent implements ActionListener {
 
         // make a hat
         g.setColor(purple);
-        g.fillRect(315, 60, 150, 200);
-        g.fillOval(245, 215, 300, 50);
+        g.fillRect(hatRectX, hatRectY, 150, 200);
+        g.fillOval(hatOvalX, hatOvalY, 300, 50);
         // put a stripe across the hat
         g.setColor(green);
-        g.fillRect(315, 180, 150, 40);
+        g.fillRect(hatStripeRectX, hatStripeRectY, 150, 40);
 
         // make eyes
         g.setColor(Color.BLACK);
         // left eye
-        g.fillOval(300, 310, 20, 20);
+        g.fillOval(leftEyeX, 310, 20, 20);
         // right eye
-        g.fillOval(450, 310, 20, 20);
+        g.fillOval(rightEyeX, 310, 20, 20);
 
         // make mouth
         g.setColor(Color.BLACK);
@@ -132,6 +155,14 @@ public class A8Q1 extends JComponent implements ActionListener {
         int[] rightToothX = {395, 415, 405};
         int[] rightToothY = {rightToothY1, rightToothY2, rightToothY3};
         g.fillPolygon(rightToothX, rightToothY, 3);
+        
+        // make lasers to shoot out of the eyes
+        g.setColor(Color.RED);
+        // left eye laser
+        g.drawLine(leftLaserX, 320, mouseX, mouseY);
+        // right eye laser
+        g.drawLine(rightLaserX, 320, mouseX, mouseY);
+        
     }
 
     // This method is used to do any pre-setup you might need to do
@@ -144,27 +175,67 @@ public class A8Q1 extends JComponent implements ActionListener {
     // In here is where all the logic for my game will go
     public void gameLoop() {
 
-        // determine whether right tooth needs to fall or go back into place
-        if (rightToothY1 <= 395) {
-            toothFalling = true;
-        } else if (rightToothY1 >= HEIGHT) {
-            toothFalling = false;
-        }
-        // make tooth fall
-        if (toothFalling == true) {
-            rightToothY1 = rightToothY1 + 2;
-            rightToothY2 = rightToothY2 + 2;
-            rightToothY3 = rightToothY3 + 2;
-        // make tooth get back into place
-        } else if (toothFalling == false) {
-            rightToothY1 = rightToothY1 - 2;
-            rightToothY2 = rightToothY2 - 2;
-            rightToothY3 = rightToothY3 - 2;
-        }
-        
-        // determine whether if hat should move left or right
-        
+        // make right tooth fall off
+        rightToothY1 = rightToothY1 + 2;
+        rightToothY2 = rightToothY2 + 2;
+        rightToothY3 = rightToothY3 + 2;
 
+        // determine whether if hat should move up or down
+        
+//        // determine whether if hat should move left or right
+//        if (hatOvalX >= 345) {
+//            hatMoveRight = false;
+//        } else if (hatOvalX <= 195) {
+//            hatMoveRight = true;
+//        }
+//        // make hat move right
+//        if (hatMoveRight == true) {
+//            hatRectX = hatRectX + 3;
+//            hatOvalX = hatOvalX + 3;
+//            hatStripeRectX = hatStripeRectX + 3;
+//        } else {
+//            // make hat move left
+//            hatRectX = hatRectX - 3;
+//            hatOvalX = hatOvalX - 3;
+//            hatStripeRectX = hatStripeRectX - 3;
+//        }
+
+        // determine whether if the left eye should move in or not
+        if (leftEyeX >= 320) {
+            leftEyeMoveIn = false;
+        } else if (leftEyeX <= 280) {
+            leftEyeMoveIn = true;
+        }
+        // make left eye move in
+        if (leftEyeMoveIn == true) {
+            leftEyeX = leftEyeX + 2;
+            // make lasers follow eye
+            leftLaserX = leftLaserX + 2;
+        } else {
+            // make left eye move out
+            leftEyeX = leftEyeX - 2;
+            // make lasers follow eye
+            leftLaserX = leftLaserX - 2;
+        }
+        
+        // determine whether if the right eye should move in or not
+        if (rightEyeX >= 470) {
+            rightEyeMoveIn = true;
+        } else if (rightEyeX <= 430) {
+            rightEyeMoveIn = false;
+        }
+        // make right eye move in
+        if (rightEyeMoveIn == true) {
+            rightEyeX = rightEyeX - 2;
+            // make lasers follow eye
+            rightLaserX = rightLaserX - 2;
+        } else {
+            // make right eye move out
+            rightEyeX = rightEyeX + 2;
+            // make lasers follow eye
+            rightLaserX = rightLaserX + 2;
+        }
+        
     }
 
     // Used to implement any of the Mouse Actions
@@ -188,6 +259,9 @@ public class A8Q1 extends JComponent implements ActionListener {
         // if the mouse has moved positions
         @Override
         public void mouseMoved(MouseEvent e) {
+            // set the mouse coordinates
+            mouseX = e.getX();
+            mouseY = e.getY();
         }
     }
 
